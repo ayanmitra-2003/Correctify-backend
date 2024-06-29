927d373ed94e4ce34a6369cea6d85622203fd558
Harsh Kumar <harsh84090kumar@gmail.com>
2024-06-29 17:05:16 +0530

    Correcting the use of cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
