126c117068624a3f1c4af52f43215579353d4ae2
Harsh Kumar <harsh84090kumar@gmail.com>
2024-06-29 15:59:42 +0530

    Removing static links from cors

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const app = express();
 
-const corsOptions = {
-    origin: ["http://localhost:5173", "http://localhost:5174"],
-};
-
-app.use(cors(corsOptions));
+app.use(cors);
 app.use(express.json());
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
